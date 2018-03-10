71a2b730d40b0113ceb6a8c111b01d2089252e3b
Stamatis Deliyannis <stamatis.deliyannis@gmail.com>
2018-03-10 12:12:37 +0200

    first commit
    Garden Stories web page

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    "use strict";
+    $.scrollify({
+        section: "section",
+        sectionName: "section-name",
+        interstitialSection: "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll: true,
+        before: function () {},
+        after: function () {},
+        afterResize: function () {},
+        afterRender: function () {}
+    });
+
+});
